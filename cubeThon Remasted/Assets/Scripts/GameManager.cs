6804b94042c8cb98f5a336c38b3911f41c1b0d47
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-01-11 17:51:38 -0700

    Add CubeThon V1
    Finised Cubethon from tutorial

---
+++ cubeThon Remasted/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	bool gameOver = false;
+	public float restartDelay = 1f;
+	public GameObject completeLevelUI;
+    public void CompleteLevel()
+	{
+		Debug.Log("LEVEL WON");
+		completeLevelUI.SetActive(true);
+	}
+    public void EndGame()
+	{
+		if (gameOver == false)
+		{
+			gameOver = true;
+			Debug.Log("Game Over");
+			Invoke("Restart", restartDelay);
+		}
+		
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
